dc73128748a11c991a9433dc3ca6be2711451a0b
Rakesh Ranjan <raks.ranjan@gmail.com>
2023-03-13 08:36:17 +0530

    Made file related updates

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from strategy.prem_100_strategy import Prem100Strategy, Prem100StrategyConfig
 import pytz
 from tabulate import tabulate
+import position_file_handler as fh
 
 current_timezone = 'Asia/Kolkata'
 tz = pytz.timezone(current_timezone)
 
 sched = BlockingScheduler(timezone=tz, job_defaults={'max_instances': 20})
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18, minute=8)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9, minute=30)
 def prepare_data():
     api = ShoonyaApiPy()
     prem_strategy = Prem100Strategy(Prem100StrategyConfig, api, None)
@@ -31,11 +32,11 @@ def execute_trade_job(api: ShoonyaApiPy, prem_strategy: Prem100Strategy):
 def track_data(prem_strategy: Prem100Strategy):
     data = prem_strategy.get_data()
     data = data[["StrikePrice", "OptionType", "Price", "Position", "Current Price", "Trigger Price", "Entry Price", "Exit Price", "Stop Loss", "PNL"]]
-    logger.info(tabulate(data, headers='keys', tablefmt='psql'))
-    logger.info(f"Total PNL = {data['PNL'].sum() * prem_strategy.config.LOT_SIZE:.2f}")
+    print(tabulate(data, headers='keys', tablefmt='psql'))
+    print(f"Total PNL = {data['PNL'].sum() * prem_strategy.config.LOT_SIZE:.2f}")
 
     if (data["Entry Price"].count() > 0) & (data["Position"].sum() == 0):
-        logger.info("All positions exited. Hence stoipping the jobs")
+        print("All positions exited. Hence stoipping the jobs")
         stop_running_job()
 
 if __name__ == "__main__":
--- position_file_handler.py
+++ position_file_handler.py
@@ -1,18 +1,18 @@
-import util
 from os import path
 import feather
+import datetime
 
-def save_position_file(name, data):
-    feather.write_dataframe(data, get_position_file_path(name))
+def save_position_file(data):
+    feather.write_dataframe(data, get_position_file_path())
 
-def position_file_exist(name):
-    return path.exists(get_position_file_path(name))
+def position_file_exist():
+    return path.exists(get_position_file_path())
 
-def read_position_file(name):
-    data = feather.read_dataframe(get_position_file_path(name))
+def read_position_file():
+    data = feather.read_dataframe(get_position_file_path())
     return data
 
-def get_position_file_path(name):
-    today_date, _ = util.get_current_date_time()
-    file_path = f'data/positions/{name.lower()}_{today_date}.feather'
+def get_position_file_path():
+    today_date = datetime.datetime.now().date()
+    file_path = f'data/positions/{today_date}_positions.feather'
     return file_path
